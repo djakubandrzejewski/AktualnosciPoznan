b63ede1e356ab151039c2545a47a63993023a0cb
djakubandrzejewski <105653548+djakubandrzejewski@users.noreply.github.com>
2023-02-10 09:09:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ access_token_secret = config['twitter']['access_token_secret']
 
 auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
+tweet_history_file = "tweet_history.txt"
 
 api = tweepy.API(auth)
+
 
 def newArticule():
     BASE_URL = requests.get('https://www.poznan.pl/').text
@@ -27,7 +29,6 @@ def newArticule():
     strToday = str(today)
     articules = soup.find_all('section', class_ ='column big')
     for articule in articules:
-        articuleDate = articule.find('time').text.replace('', '')
         if strToday in articuleDate:
             articuleLink = articule.find('a', href=True)
             articuleLink = articuleLink['href']
@@ -42,42 +43,78 @@ def newArticule():
                 f.write(articuleImageDownload_response.content)
 
 
-            
-
             with open('articules/articules.txt', 'w', encoding='utf-8') as f:
-                f.write((f'{articule_title.strip()}\n{articuleDate.strip()}\nhttps://www.poznan.pl{articuleLink}'))
+                f.write((f'{articule_title.strip()}\n{articuleDate.strip()}\n{urlPoznan}{articuleLink}'))
+
+            with open('tweet_history_file', 'w') as f:
+                f.write(f'{articule_title}')
+
+
+def newAccident():
+    BASE_URL = requests.get('https://www.poznan.pl').text
+    soup = BeautifulSoup(BASE_URL, 'lxml')
+    today = date.today()
+    strToday = str(today)
+    failures = soup.find('article', class_='dotdotdot awarie-news-text')       
+    accidentTitle = failures.find('h3').text.strip()
+    accidentLink = failures.find('a')
+    accidentLink = accidentLink['href']
+    urlPoznan = 'http://poznan.pl/'
+    accidentCategory = failures.find('div', class_='label-awarie').text
+    with open('failures/failures.txt', 'w', encoding='utf8') as f:
+        f.write(f'{accidentTitle.strip()}\n{accidentCategory} {urlPoznan}{accidentLink}')   
+    with open('tweet_history_file', 'w') as f:
+        f.write(f'{accidentTitle}')
+
+
+
+
 
 def sendArticule(articule):
     old_articule = newArticule()
-    if articule != old_articule:
-        api.update_status(f'Artykuł: {articuleTitle} opublikowany: {articuleDate}\n Link: {articuleLink}')
+    if id(articule) != id(old_articule):
+        api.update_status(f'{articuleTitle} opublikowany: {articuleDate}\n Link: {articuleLink}')
         
-
+def sendAccident(failure):
+    old_failure = newAccident()
+    if id(failure) != id(old_failure):
+        api.update_status((f'{accidentTitle} opublikowany: {accidentDate}\n Link: {accidentLink}'))
+    
+def add_tweet(tweet):
+    for existing_tweet in tweets:
+        if existing_tweet == tweet:
+            return
+    tweets.append(tweet)
 
 
 if __name__ == '__main__':
+    sent_tweets = set()
+    tweet_history = set()
     while True:
-        newArticule()
         time_wait = 1
         print(f'Sprawdzania nowego artykulu za: {time_wait} sekund.')
         time.sleep(1)
+        tweets = []
         with open('articules/articules.txt', 'r', encoding='utf8') as f:
             articuleTitle = f.readline().strip()
             articuleDate = f.readline().strip()
             articuleLink = f.readline().strip()
+        with open('failures/failures.txt', 'r', encoding='utf8') as f:
+            accidentTitle = f.readline().strip()
+            accidentDate = f.readline().strip()
+            accidentLink = f.readline().strip()
         
-
-
-        try:
-            sendArticule(newArticule)
-        except tweepy.errors.Forbidden:
-            time.sleep(1)
-
+        if articuleTitle not in sent_tweets:
+            try:
+                sendArticule(newArticule)
+                sent_tweets.add(articuleTitle)
+            except tweepy.errors.Forbidden:
+                time.sleep(1)
+        
+        if accidentTitle not in sent_tweets:
+            try:
+                sendAccident(newAccident)
+                sent_tweets.add(accidentTitle)
+            except tweepy.errors.Forbidden:
+                time.sleep(1)
             
-
-
-
-
-# 600 - 10 minut
-# 60 -  1 minut
-# 6 - 1 sekunda
